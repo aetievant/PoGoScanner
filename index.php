440c91790ccebb623d29651455977f81e145e775
Antoine ETIEVANT <antoine.etievant@efipeek.com>
2017-04-21 19:10:06 +0200

    Updated index.php since API no longer return Pokémon IVs

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 require(dirname(__FILE__).'/config/config.inc.php');
 
-$sql = 'SELECT sp.pokemon_id, p.french_name, sp.expiration_date, CONCAT(sp.latitude,\',\',sp.longitude) AS gps, (sp.iv_attack + sp.iv_defense + sp.iv_stamina) / .45 AS IV, sp.iv_attack, sp.iv_defense, sp.iv_stamina, z.name
+$sql = 'SELECT sp.pokemon_id, p.french_name, sp.expiration_date, CONCAT(sp.latitude,\',\',sp.longitude) AS gps, z.name
 FROM spawn_point sp
 NATURAL JOIN pokemon p
 NATURAL JOIN zone z
@@ -28,10 +28,6 @@ $res = Db::getInstance()->executeS($sql);
                     <th>Name</th>
                     <th>Expiration</th>
                     <th>GPS</th>
-                    <th>IV</th>
-                    <th>IV attack</th>
-                    <th>IV defense</th>
-                    <th>IV stamina</th>
                     <th>Zone name</th>
                 </tr>
             </thead>
